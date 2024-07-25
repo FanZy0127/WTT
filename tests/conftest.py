367a3105327e28bfabdaf69a2cd36ec6382a9b9b
baptiste-3wa <baptiste@3wa.io>
2024-07-25 16:38:32 +0200

    Update of requirements to add test coverage bundle. Update of .gitignore for tests/__pycache__. First update of tests, still few async tests to work on.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,25 +6,22 @@ from app.models import Base
 
 DATABASE_URL_TEST = "sqlite+aiosqlite:///./test.db"
 
-
 @pytest.fixture(scope="session")
 def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
-
 @pytest.fixture(scope="session")
-def async_engine():
+async def async_engine():
     engine = create_async_engine(DATABASE_URL_TEST, echo=True)
-    yield engine
-    engine.dispose()
-
-
-@pytest.fixture(scope="session")
-async def async_session(async_engine):
-    async with async_engine.begin() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    yield engine
+    await engine.dispose()
+
+@pytest.fixture(scope="function")
+async def async_session(async_engine):
     async_session_factory = sessionmaker(
         bind=async_engine,
         class_=AsyncSession,
@@ -34,5 +31,3 @@ async def async_session(async_engine):
     )
     async with async_session_factory() as session:
         yield session
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
